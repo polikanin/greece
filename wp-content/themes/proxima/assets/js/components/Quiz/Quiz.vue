68c0162758c27f38e4832fa7f5df500bc46b0c5c
apolikanin <apolikanin@webiprog.de>
2024-09-11 01:43:54 +0200

    change fields

--- wp-content/themes/proxima/assets/js/components/Quiz/Quiz.vue
+++ wp-content/themes/proxima/assets/js/components/Quiz/Quiz.vue
@@ -2,18 +2,11 @@
     <vp-modal v-show="isOpen">
         <template #header>
             <div class="vp-modal--header" v-show="step < 5">
-                <h2 class="vp-modal--title" v-show="step === 0">
-                    Initial Onboarding Questionnaire
-                </h2>
                 <transition>
                     <div class="vp-modal--steps" v-show="step !== 0 && step < 5">
                         <div class="vp-modal--step" :class="{active: step === 1, passed: step > 1}">01</div>
                         <div class="vp-modal--step-line"></div>
                         <div class="vp-modal--step" :class="{active: step === 2, passed: step > 2}">02</div>
-                        <div class="vp-modal--step-line"></div>
-                        <div class="vp-modal--step" :class="{active: step === 3, passed: step > 3}">03</div>
-                        <div class="vp-modal--step-line"></div>
-                        <div class="vp-modal--step" :class="{active: step === 4, passed: step > 4}">04</div>
                     </div>
                 </transition>
                 <div class="vp-modal--close" @click="close" v-show="step !== 0">
@@ -25,40 +18,6 @@
             </div>
         </template>
         <template #body>
-            <transition>
-                <div class="vp-modal--body" v-show="step === 0">
-                    <p class="vp-modal--subtitle">
-                        We want to ensure we provide you with a tailor made and efficient service.
-                        Please take the following 1 minute to provide us with necessary information. One of our advisors
-                        will be in
-                        touch with you within 24 hours.
-                        Confidentiality and trust are at the core of our values. No information will be shared with
-                        third parties.
-                    </p>
-
-                    <div class="vp-modal--terms">
-                        <label class="vp-checkbox">
-                            <input type="checkbox" v-model="quiz.terms">
-                            <span class="vp-checkbox--box"></span>
-                            <span class="vp-checkbox--text">
-            I agree to the
-            <a :href="terms" target="_blank">Terms and Conditions </a>
-            <b>and </b>
-            <a :href="privacy" target="_blank">Privacy Policy</a>
-          </span>
-                        </label>
-
-                        <div class="vp-error--msg" v-if="v$.quiz.terms.$error">{{ msg.terms }}</div>
-                    </div>
-                    <div class="vp-modal--navigation">
-                        <vp-button @click="submitStep">
-                            <span class="vp-btn--text">Press to start</span>
-                            <vp-icon type="arrow-next"></vp-icon>
-                        </vp-button>
-                    </div>
-                </div>
-            </transition>
-
             <transition>
                 <div class="vp-modal--body" v-show="step === 1">
                     <div class="vp-modal--group">
@@ -104,70 +63,6 @@
                         </div>
                     </div>
 
-                    <div class="vp-modal--block" :class="{'vp-input--error': v$.quiz.videoCall.$error}">
-                        <div class="vp-modal--block-label">
-                            Would you like to schedule a complimentary video call with an allocated advisor?
-                        </div>
-                        <div class="vp-modal--block-container">
-                            <label class="vp-radio">
-                                <input type="radio" value="Yes" name="videoCall" v-model="quiz.videoCall">
-                                <span class="vp-radio--box"></span>
-                                <span class="vp-radio--text">Yes</span>
-                            </label>
-                            <label class="vp-radio">
-                                <input type="radio" value="No" name="videoCall" v-model="quiz.videoCall">
-                                <span class="vp-radio--box"></span>
-                                <span class="vp-radio--text">No</span>
-                            </label>
-                        </div>
-
-                        <div class="vp-error--msg" v-if="v$.quiz.videoCall.$error">{{ msg.videoCall }}</div>
-                    </div>
-
-                    <div class="vp-modal--block" :class="{'vp-input--error': v$.quiz.whatsAppCall.$error}">
-                        <div class="vp-modal--block-label">
-                            May we contact you via WhatsApp?
-                        </div>
-                        <div class="vp-modal--block-container">
-                            <label class="vp-radio">
-                                <input type="radio" value="Yes" name="whatsAppCall" v-model="quiz.whatsAppCall">
-                                <span class="vp-radio--box"></span>
-                                <span class="vp-radio--text">Yes</span>
-                            </label>
-                            <label class="vp-radio">
-                                <input type="radio" value="No" name="whatsAppCall" v-model="quiz.whatsAppCall">
-                                <span class="vp-radio--box"></span>
-                                <span class="vp-radio--text">No</span>
-                            </label>
-                        </div>
-
-                        <div class="vp-error--msg" v-if="v$.quiz.whatsAppCall.$error">{{ msg.whatsAppCall }}</div>
-                    </div>
-
-                    <div class="vp-modal--navigation">
-<!--                        <vp-button btn-class="vp-btn&#45;&#45;simple" @click="stepBack">-->
-<!--                            <vp-icon type="arrow-prev"></vp-icon>-->
-<!--                            <span class="vp-btn&#45;&#45;text">Go Back</span>-->
-<!--                        </vp-button>-->
-                        <vp-button @click="submitStep">
-                            <span class="vp-btn--text">Next Step</span>
-                            <vp-icon type="arrow-next"></vp-icon>
-                        </vp-button>
-                    </div>
-                </div>
-            </transition>
-
-            <transition>
-                <div class="vp-modal--body" v-show="step === 2">
-                    <div class="vp-modal--group">
-              <textarea class="vp-textarea"
-                        v-model="quiz.message"
-                        :class="{'vp-textarea--error': v$.quiz.message.$error}"
-                        placeholder="Family composition of the applicants including age?"></textarea>
-
-                        <div class="vp-error--msg" v-if="v$.quiz.message.$error">{{ msg.message }}</div>
-                    </div>
-
                     <div class="vp-modal--group">
                         <div class="vp-input--group">
                             <div class="vp-input--preview">
@@ -187,6 +82,42 @@
                         <div class="vp-error--msg" v-if="v$.quiz.timelineCity.$error">{{ msg.timelineCity }}</div>
                     </div>
 
+                    <div class="vp-modal--navigation">
+                        <!--                        <vp-button btn-class="vp-btn&#45;&#45;simple" @click="stepBack">-->
+                        <!--                            <vp-icon type="arrow-prev"></vp-icon>-->
+                        <!--                            <span class="vp-btn&#45;&#45;text">Go Back</span>-->
+                        <!--                        </vp-button>-->
+                        <vp-button @click="submitStep">
+                            <span class="vp-btn--text">Next Step</span>
+                            <vp-icon type="arrow-next"></vp-icon>
+                        </vp-button>
+                    </div>
+                </div>
+            </transition>
+
+            <transition>
+                <div class="vp-modal--body" v-show="step === 2">
+                    <div class="vp-modal--label">
+                        I have the financial capability to invest a minimum of €400,000 or €800,000 plus necessary fees.
+                    </div>
+                    <div class="vp-modal--group" v-for="item in fields.capability">
+                        <label class="vp-radio">
+                            <input type="radio" name="capability" :value="item" v-model="quiz.capability">
+                            <span class="vp-radio--box"></span>
+                            <span class="vp-radio--text">{{ item }}</span>
+                        </label>
+                    </div>
+                    <div class="vp-error--msg" v-if="v$.quiz.capability.$error">{{ msg.capability }}</div>
+
+                    <div class="vp-modal--group">
+              <textarea class="vp-textarea"
+                        v-model="quiz.message"
+                        :class="{'vp-textarea--error': v$.quiz.message.$error}"
+                        placeholder="Family composition of the applicants including age?"></textarea>
+
+                        <div class="vp-error--msg" v-if="v$.quiz.message.$error">{{ msg.message }}</div>
+                    </div>
+
                     <div class="vp-modal--label">
                         What is the motivation behind obtaining the Greek Golden Visa?
                         <span>* Choose one or more options</span>
@@ -200,55 +131,11 @@
                             <span class="vp-checkbox--box"></span>
                             <span class="vp-checkbox--text">{{ item }}</span>
                         </label>
-
                     </div>
-
                     <div class="vp-error--msg" v-if="v$.quiz.motivation.$error">{{ msg.motivation }}</div>
 
-                    <div class="vp-modal--navigation">
-                        <vp-button btn-class="vp-btn--simple" @click="stepBack">
-                            <vp-icon type="arrow-prev"></vp-icon>
-                            <span class="vp-btn--text">Go Back</span>
-                        </vp-button>
-                        <vp-button @click="submitStep">
-                            <span class="vp-btn--text">Next Step</span>
-                            <vp-icon type="arrow-next"></vp-icon>
-                        </vp-button>
-                    </div>
-                </div>
-            </transition>
-
-            <transition>
-                <div class="vp-modal--body" v-show="step === 3">
                     <div class="vp-modal--label">
-                        What are your objectives in regards to the investment vehicle chosen?
-                        <span>*Up to two responses</span>
-                    </div>
-                    <div class="vp-modal--group" v-for="item in fields.objectives">
-                        <label class="vp-checkbox">
-                            <input type="checkbox"
-                                   :value="item"
-                                   v-model="quiz.objectives">
-                            <span class="vp-checkbox--box"></span>
-                            <span class="vp-checkbox--text">{{ item }}</span>
-                        </label>
-                    </div>
-                    <div class="vp-error--msg" v-if="v$.quiz.objectives.$error">{{ msg.objectives }}</div>
-
-                    <div class="vp-modal--label">
-                        How would you describe your risk profile?
-                    </div>
-                    <div class="vp-modal--group" v-for="item in fields.risk">
-                        <label class="vp-radio">
-                            <input type="radio" :value="item" name="risk" v-model="quiz.risk">
-                            <span class="vp-radio--box"></span>
-                            <span class="vp-radio--text">{{ item }}</span>
-                        </label>
-                    </div>
-                    <div class="vp-error--msg" v-if="v$.quiz.risk.$error">{{ msg.risk }}</div>
-
-                    <div class="vp-modal--label">
-                        Preferred type of investment?
+                        What is your preferred type of investment?
                         <span>* Up to two responses</span>
                     </div>
 
@@ -265,45 +152,6 @@
                         </label>
                     </div>
                     <div class="vp-error--msg" v-if="v$.quiz.investment.$error">{{ msg.investment }}</div>
-
-                    <div class="vp-modal--navigation">
-                        <vp-button btn-class="vp-btn--simple" @click="stepBack">
-                            <vp-icon type="arrow-prev"></vp-icon>
-                            <span class="vp-btn--text">Go Back</span>
-                        </vp-button>
-                        <vp-button @click="submitStep">
-                            <span class="vp-btn--text">Next Step</span>
-                            <vp-icon type="arrow-next"></vp-icon>
-                        </vp-button>
-                    </div>
-                </div>
-            </transition>
-
-            <transition>
-                <div class="vp-modal--body" v-show="step === 4">
-                    <div class="vp-modal--label">
-                        What are your investment thresholds?
-                    </div>
-                    <div class="vp-modal--group" v-for="item in fields.capability">
-                        <label class="vp-radio">
-                            <input type="radio" name="capability" :value="item" v-model="quiz.capability">
-                            <span class="vp-radio--box"></span>
-                            <span class="vp-radio--text">{{ item }}</span>
-                        </label>
-                    </div>
-                    <div class="vp-error--msg" v-if="v$.quiz.capability.$error">{{ msg.capability }}</div>
-
-                    <div class="vp-modal--label">
-                        Are you planning on relocating to Greece?
-                    </div>
-                    <div class="vp-modal--group" v-for="item in fields.relocating">
-                        <label class="vp-radio">
-                            <input type="radio" name="relocating" :value="item" v-model="quiz.relocating">
-                            <span class="vp-radio--box"></span>
-                            <span class="vp-radio--text">{{ item }}</span>
-                        </label>
-                    </div>
-                    <div class="vp-error--msg" v-if="v$.quiz.relocating.$error">{{ msg.relocating }}</div>
 
                     <div class="vp-modal--label">
                         Time frame to investment?
@@ -331,7 +179,7 @@
             </transition>
 
             <transition>
-                <div class="vp-modal--body" v-show="step === 5">
+                <div class="vp-modal--body" v-show="step === 3">
                     <h2 class="vp-modal--heading">
                         Thank you!
                     </h2>
@@ -368,21 +216,19 @@ export default {
     props: ['isOpen', 'terms', 'privacy'],
     data() {
         return {
-            step: 0,
+            step: 1,
             selected: false,
             selectedTimeline: false,
             code: window.VpPhoneCode,
             timeline: window.VpTimeline,
             fields: window.VpFormFields,
             quiz: {
-                terms: false,
+                terms: true,
                 firstName: '',
                 lastName: '',
                 email: '',
                 phone: '',
                 phoneCode: '',
-                videoCall: '',
-                whatsAppCall: '',
                 message: '',
                 timelineCode: '',
                 timelineCity: '',
@@ -405,8 +251,6 @@ export default {
                 lastName: {required},
                 email: {required, email},
                 phone: {required},
-                videoCall: {required},
-                whatsAppCall: {required},
                 message: {required},
                 timelineCity: {required},
                 motivation: {required},
@@ -418,17 +262,14 @@ export default {
                 frameToInvestment: {required},
             },
             $validationGroups: {
-                step_0: ['quiz.terms'],
-                step_1: ['quiz.firstName', 'quiz.lastName', 'quiz.email', 'quiz.phone', 'quiz.videoCall', 'quiz.whatsAppCall'],
-                step_2: ['quiz.message', 'quiz.timelineCity', 'quiz.motivation'],
-                step_3: ['quiz.objectives', 'quiz.risk', 'quiz.investment'],
-                step_4: ['quiz.capability', 'quiz.relocating', 'quiz.frameToInvestment'],
+                step_1: ['quiz.firstName', 'quiz.lastName', 'quiz.email', 'quiz.phone', 'quiz.timelineCity'],
+                step_2: ['quiz.capability', 'quiz.message', 'quiz.motivation', 'quiz.investment', 'quiz.frameToInvestment'],
             }
         }
     },
     mounted() {
         let self = this;
-        document.body.addEventListener('clear', function (){
+        document.body.addEventListener('clear', function () {
             self.resetForm()
         })
     },
@@ -460,7 +301,7 @@ export default {
             let self = this
             if (this.v$.$validationGroups['step_' + this.step]) {
                 if (!this.v$.$validationGroups['step_' + this.step].$invalid) {
-                    if(this.step > 0){
+                    if (this.step > 0) {
                         this.sendForm(true)
                     }
 
